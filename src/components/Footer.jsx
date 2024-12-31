8b3c67bb676b5873eb0b3f12a10199e7c181cec0
raigomes <raigomes2@hotmail.com>
2024-12-31 01:01:36 -0300

    Add Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Footer.module.css'
+import Dog from '../img/dog.svg?react'
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>Footer</footer>
+    <footer className={styles.footer}>
+      <Dog />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
   )
 }
 
