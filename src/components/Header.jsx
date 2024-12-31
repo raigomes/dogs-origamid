5e5db027a507d59fe8f7a3e2b8021895a49313cc
raigomes <raigomes2@hotmail.com>
2024-12-31 00:52:52 -0300

    Add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./Header.module.css";
+import Dog from '../img/dog.svg?react'
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" end className={styles.logo}>
+            <Dog />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
       </nav>
     </header>
   );
