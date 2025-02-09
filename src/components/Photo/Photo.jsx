bff557a8e4412c43b4a254cc99755d08898e0000
raigomes <raigomes2@hotmail.com>
2025-02-08 23:50:59 -0300

    Add button delete

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Photo.module.css";
 import FeedPhoto from "../Feed/FeedPhoto";
 import PhotoComments from "./PhotoComments";
 import PhotoDetails from "./PhotoDetails";
+import { useLogin } from "../../hooks/useLogin";
 
 const Photo = ({ data, id }) => {
+  const [isUserPhoto, setIsUserPhoto] = useState(false)
+  const { getUser } = useLogin()
+  
+  useEffect(() => {
+    async function updateUserPhoto () {
+      const user = await getUser()
+      setIsUserPhoto(user.nome === data.author)
+    }
+    updateUserPhoto()
+  }, [isUserPhoto])
+
   if (!data) return null;
   return (
     <div className={`${styles.photo} ${id ? styles.single : ""}`}>
@@ -12,7 +24,7 @@ const Photo = ({ data, id }) => {
         <FeedPhoto src={data.src} alt={data.title} />
       </div>
       <div className={styles.details}>
-        <PhotoDetails data={data} />
+        <PhotoDetails data={data} isUserPhoto={isUserPhoto} />
       </div>
       <PhotoComments id={data.id} />
     </div>
--- src/components/Photo/PhotoDetails.jsx
+++ src/components/Photo/PhotoDetails.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import styles from "./Photo.module.css"
-import { Link } from "react-router-dom";
+import styles from "./Photo.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import { PHOTO_DELETE } from "../../api/services";
+import { useLogin } from "../../hooks/useLogin";
 
-const PhotoDetails = ({data}) => {
-  if(!data) return null
+const PhotoDetails = ({ data, isUserPhoto }) => {
+  const { getToken } = useLogin()
+  const navigate = useNavigate()
+
+  async function handleDelete() {
+    try {
+      if(window.confirm("Você quer realmente deletar a foto?")){
+        const token = getToken()
+        const { endpoint, method, headers } = PHOTO_DELETE(data.id, token);
+        const response = await fetch(endpoint, { method, headers })
+        if(response.ok) navigate("/")
+      }
+    } catch(e) {
+      console.error(e)
+    }
+  }
+
+  if (!data) return null;
   return (
     <div>
       <p className={styles.author}>
-        <Link to={`/perfil/${data.author}`}>@{data.author}</Link>
+        {!isUserPhoto && (
+          <Link to={`/perfil/${data.author}`}>@{data.author}</Link>
+        )}
+        {isUserPhoto && (
+          <button className={styles.delete} onClick={handleDelete}>
+            Deletar
+          </button>
+        )}
         <span className={styles.visualizacoes}>{data.acessos}</span>
       </p>
       <h1 className="title">
