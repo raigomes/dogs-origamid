1f14412922e1f19f3d4b5f3c2dc0cc5046a047f5
raigomes <raigomes2@hotmail.com>
2025-02-09 15:48:38 -0300

    Implement Profile feed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Feed from './Feed/Feed'
-import { useParams } from 'react-router-dom';
-import Head from './Head';
+import React from "react";
+import FeedList from "./Feed/FeedList";
+import { useParams } from "react-router-dom";
+import Head from "./Head";
 
 const Profile = () => {
-    const params = useParams();
+  const params = useParams();
 
   return (
     <section className="container mainSection">
-        <Head title={`${params.name} | Dogs`} description="" />
-        <h1 className="title">{params.name}</h1>
-        <Feed />
+      <Head title={`${params.name} | Dogs`} description="" />
+      <h1 className="title">{params.name}</h1>
+      <FeedList user={params.name} />
     </section>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
