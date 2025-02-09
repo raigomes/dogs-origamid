c1b0bed8696446a1831573c113959119ab88845c
raigomes <raigomes2@hotmail.com>
2025-02-09 03:26:59 -0300

    Add NavMobile

--- src/components/Conta/Conta.jsx
+++ src/components/Conta/Conta.jsx
@@ -1,19 +1,13 @@
-import { useContext, useEffect, useState } from "react";
-import styles from "./Conta.module.css";
-import { NavLink, Outlet, useLocation } from "react-router-dom";
-import Dashboard from "../../img/dashboard.svg?react";
-import Bars from "../../img/bars.svg?react";
-import Plus from "../../img/plus.svg?react";
-import Exit from "../../img/exit.svg?react";
+import { useContext, useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
-import Head from "../Head";
 import { useLogin } from "../../hooks/useLogin";
+import ContaHeader from "./ContaHeader";
+import Head from "../Head";
 
 const Conta = () => {
-  const [title, setTitle] = useState("");
   const { loggedIn, sair } = useContext(UserContext);
   const { getToken, validateToken } = useLogin();
-  const location = useLocation();
 
   useEffect(() => {
     async function redirectLogin() {
@@ -25,44 +19,12 @@ const Conta = () => {
     redirectLogin()
   }, [])
 
-  useEffect(() => {
-    switch (location.pathname) {
-      case "/conta":
-        setTitle("Minha Conta");
-        break;
-      case "/conta/estatisticas":
-        setTitle("Estatísticas");
-        break;
-      case "/conta/postar":
-        setTitle("Poste Sua Foto");
-        break;
-      default:
-        throw new Error("Rota Inválida");
-    }
-  }, [location]);
-
-  if(!loggedIn || !title) return null
+  if(!loggedIn) return null
 
   return (
     <section className="container">
       <Head title="Minha Conta | Dogs" description="" />
-      <header className={styles.header}>
-        <h1 className="title">{title}</h1>
-        <nav className={styles.nav}>
-          <NavLink to="/conta" end>
-            <Dashboard />
-          </NavLink>
-          <NavLink to="/conta/estatisticas">
-            <Bars />
-          </NavLink>
-          <NavLink to="/conta/postar">
-            <Plus />
-          </NavLink>
-          <button onClick={sair}>
-            <Exit />
-          </button>
-        </nav>
-      </header>
+      <ContaHeader />
       <Outlet />
     </section>
   );
---
+++ src/components/Conta/ContaHeader.jsx
@@ -0,0 +1,100 @@
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./Conta.module.css";
+import { NavLink, useLocation } from "react-router-dom";
+import Dashboard from "../../img/dashboard.svg?react";
+import Bars from "../../img/bars.svg?react";
+import Plus from "../../img/plus.svg?react";
+import Exit from "../../img/exit.svg?react";
+import { UserContext } from "../../context/UserContext";
+
+const NavDesktop = ({ sair }) => (
+  <nav className={styles.nav}>
+    <NavLink to="/conta" end>
+      <Dashboard />
+    </NavLink>
+    <NavLink to="/conta/estatisticas">
+      <Bars />
+    </NavLink>
+    <NavLink to="/conta/postar">
+      <Plus />
+    </NavLink>
+    <button onClick={sair}>
+      <Exit />
+    </button>
+  </nav>
+);
+
+const NavMobile = ({ sair }) => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <>
+      <button
+        aria-label="Menu"
+        class={`${styles.mobileButton} ${active ? styles.mobileButtonActive : false}`}
+        onClick={() => setActive(!active)}
+      ></button>
+      <nav class={`${styles.navMobile} ${active ? styles.navMobileActive : false}`}>
+        <NavLink to="/conta" end>
+          <Dashboard />
+          Minhas Fotos
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Bars />
+          Estatísticas
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <Plus />
+          Adicionar Foto
+        </NavLink>
+        <button onClick={sair}>
+          <Exit />
+          Sair
+        </button>
+      </nav>
+    </>
+  );
+};
+
+const ContaHeader = () => {
+  const [title, setTitle] = useState("");
+  const [nav, setNav] = useState(null);
+  const { sair } = useContext(UserContext);
+  const location = useLocation();
+
+  useEffect(() => {
+    function chooseNav() {
+      if (window.innerWidth > 640) setNav(<NavDesktop sair={sair} />);
+      else setNav(<NavMobile sair={sair} />);
+    }
+    window.addEventListener("resize", chooseNav);
+    chooseNav();
+  }, []);
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/conta":
+        setTitle("Minha Conta");
+        break;
+      case "/conta/estatisticas":
+        setTitle("Estatísticas");
+        break;
+      case "/conta/postar":
+        setTitle("Poste Sua Foto");
+        break;
+      default:
+        throw new Error("Rota Inválida");
+    }
+  }, [location]);
+
+  if (!title) return null;
+
+  return (
+    <header className={styles.header}>
+      <h1 className="title">{title}</h1>
+      {nav}
+    </header>
+  );
+};
+
+export default ContaHeader;
